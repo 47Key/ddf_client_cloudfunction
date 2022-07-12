115e69a7d4bc29c8defc338cd5e6c16140764401
47key <rkeegan@live.com>
2022-07-12 14:35:56 -0400

    Working firebase cloud function, acting as a ddf_client :tada:

---
+++ functions/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const rets = require('./rets');
+const {onRequest} = require("firebase-functions/v2/https");
+
+const app = express();
+
+app.get('/login', async (req, res) => {
+    await rets.login();
+    res.send(JSON.stringify({
+        message: "Logged in"
+    }));
+});
+
+app.get('/logout', async (req, res) => {
+    await rets.logout();
+    res.send(JSON.stringify({
+        message: "Logged out"
+    }));
+});
+
+app.get('/', async (req, res) => {
+    await rets.search((err, ret) => {
+        if (err) console.log('Error : ', err);
+        else res.send(ret);
+    }, '*');
+});
+
+app.get('/:id', async (req, res) => {
+    await rets.search((err, ret) => {
+        if (err) console.log('Error : ', err);
+        else res.send(ret);
+    }, req.params.id);
+});
+
+app.get('/:id/:type', async (req, res) => {
+    await rets.object((err, ret) => {
+        if (err) {
+            console.log('Error : ', err);
+            return res.status(501).send(JSON.stringify({
+                message: "Could not complete"
+            }));
+        }
+        else res.send(ret);
+    }, req.params.id, req.params.type);
+});
+
+// app.listen(3000, () => console.log('Rets app listening on port 3000 ...'));
+exports.app = onRequest(app);
---
+++ functions/rets.js
@@ -0,0 +1,55 @@
+var { RetsClient, RetsVersion } = require('rets-ddf-client');
+ 
+const client = new RetsClient({
+    url: 'https://data.crea.ca/Login.svc/Login',
+    username: '0sBxf1qckQ1skE2uWfCtXDzz',
+    password: 'RNwPqqvc3f7vokyh0JkbZRSC',
+    version: RetsVersion.CREA_DDF
+});
+
+async function login() {
+    await client.login();
+}
+
+async function search(callback, id) {
+    try {
+        await client.search({
+            format: 'Standard-XML',
+            query: `(ID=${id})`,
+            searchType: 'Property',
+            class: 'Property',
+            culture: 'en-CA'
+        }).then(result => {
+            callback(null, result)
+        })
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+async function object(callback, id, type) {
+    try {
+        await client.getObjects({
+            resource: 'Property',
+            type: `${type}`,
+            contentId: `${id}`,
+            objectId: '1',
+            withLocation: false
+        }).then(result => {
+            callback(null, result)
+        });
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+async function logout() {
+    await client.logout();
+}
+
+module.exports = {
+    login,
+    logout,
+    search,
+    object
+}
